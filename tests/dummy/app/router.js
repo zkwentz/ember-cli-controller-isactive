f608953b20c2c57359e18d7e5dda352448250f7f
Zach Wentz <zwentz@netflix.com>
2015-05-12 17:19:26 -0700

    fix tests for 1.10 thanks @stefanpenner

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -5,8 +5,10 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-export default Router.map(function() {
+Router.map(function() {
   this.route('about');
   this.route('posts');
   this.route('generated');
 });
+
+export default Router;
